4d9ded2b426961490d6f5d5fcc326c3013d77fe0
Ilya Labovich <labovichl333@mail.ru>
2023-01-28 21:26:17 +0300

    Task 2.7: Implemented Cart page

--- web/src/main/java/com/es/phoneshop/web/controller/pages/CartPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/CartPageController.java
@@ -1,25 +1,68 @@
 package com.es.phoneshop.web.controller.pages;
 
+import com.es.core.cart.Cart;
 import com.es.core.cart.CartService;
+import com.es.phoneshop.web.controller.pages.dto.AddToCartRequestDto;
+import com.es.phoneshop.web.controller.pages.dto.UpdateCartDto;
+import com.es.phoneshop.web.controller.validation.AddToCartRequestDtoValidator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/cart")
 public class CartPageController {
     @Resource
     private CartService cartService;
+    @Resource
+    private AddToCartRequestDtoValidator validator;
 
     @RequestMapping(method = RequestMethod.GET)
-    public void getCart() {
-
+    public String getCart(Model model, HttpSession session) {
+        Cart cart = cartService.getCart(session);
+        model.addAttribute("cart", cart);
+        model.addAttribute("cartItems", mapCartItemsToDto(cart));
+        return "cart";
     }
 
-    @RequestMapping(method = RequestMethod.PUT)
-    public void updateCart() {
-
+    @RequestMapping(method = RequestMethod.POST)
+    public String updateCart(@Valid @ModelAttribute("cartItems") UpdateCartDto updateCartDto,
+                             BindingResult br, HttpSession session, Model model) {
+        validator.validate(updateCartDto, br);
+        if (br.hasErrors()) {
+            return "cart";
+        }
+        Cart cart = cartService.getCart(session);
+        updateCartDto.getCartItems()
+                .forEach(crtItem -> cartService.update(cart, crtItem.getPhoneId(), Long.valueOf(crtItem.getQuantity())));
+        return "redirect:/cart";
     }
+
+    @RequestMapping(value = "/delete/{phoneId}", method = RequestMethod.DELETE)
+    public String deleteCartItem(Model model, HttpSession session, @PathVariable Long phoneId) {
+        Cart cart = cartService.getCart(session);
+        cartService.remove(cart, phoneId);
+        model.addAttribute("cartItems", cart.getItems());
+        return "redirect:/cart";
+    }
+
+
+    private UpdateCartDto mapCartItemsToDto(Cart cart) {
+        List<AddToCartRequestDto> dtoElement = new ArrayList<>();
+        cart.getItems().forEach(cartItem -> dtoElement
+                .add(new AddToCartRequestDto(cartItem.getPhone().getId(), String.valueOf(cartItem.getQuantity()))));
+        UpdateCartDto dto = new UpdateCartDto(dtoElement);
+        return dto;
+    }
+
 }
---
+++ web/src/main/webapp/WEB-INF/pages/cart.jsp
@@ -0,0 +1,82 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<html>
+<head>
+    <title>ProductList</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/styles/pagination.css">
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/styles/main.css">
+</head>
+<body>
+<p><a href="#">Login</a></p>
+<p>
+    My cart: <span id="cartState">${cart.totalQuantity} items ${cart.totalCost}</span>$
+</p>
+<p><a href="${pageContext.request.contextPath}/productList/1">Back to product list</a></p>
+<p>
+</p>
+<p>
+<table border="1px">
+    <thead>
+    <tr>
+        <td>Image</td>
+        <td>
+            Brandа
+        </td>
+        <td>Model</td>
+        <td>Color</td>
+        <td>Display size</td>
+        <td>
+            Price
+        </td>
+        <td>Quantity</td>
+        <td>Action</td>
+    </tr>
+    </thead>
+
+    <form:form method="post" modelAttribute="cartItems" id="update-form">
+        <c:forEach var="cartItem" items="${cartItems.cartItems}" varStatus="status">
+            <c:set var="phone" value="${cart.items[status.index].phone}"/>
+            <tr>
+                <td>
+                    <img src="https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/${phone.imageUrl}">
+                </td>
+                <td>${phone.brand}</td>
+                <td>${phone.model}</td>
+                <td>
+                    <c:forEach var="color" items="${phone.colors}">
+                        ${color.code},
+                    </c:forEach>
+                </td>
+                <td>${phone.displaySizeInches}</td>
+                <td>${phone.price}$</td>
+                <td>
+                    <c:set var="i" value="${status.index}"/>
+                    <form:hidden path="cartItems[${i}].phoneId"/>
+                    <form:input path="cartItems[${i}].quantity"/><br>
+                    <form:errors path="cartItems[${i}].quantity" cssClass="error"/>
+                </td>
+                <td>
+                    <button form="delete" formaction="<c:url value = "/cart/delete/${phone.id}" />">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+
+        </c:forEach>
+        <p>
+            <input type="submit" value="Update">
+        </p>
+    </form:form>
+
+    <form:form id="delete" method="delete">
+    </form:form>
+</table>
+</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="${pageContext.servletContext.contextPath}/js/main.js"></script>
+</body>
+</html>
